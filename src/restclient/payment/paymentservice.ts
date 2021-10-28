3f7a8a43389d89d7b0c0fd7af5201d256d1c0f2e
carl.duranleau <carl.duranleau@ticketmaster.com>
2021-10-28 08:49:48 -0400

    Implemented PaymentService

--- src/restclient/payment/paymentservice.ts
+++ src/restclient/payment/paymentservice.ts
@@ -6,7 +6,7 @@ import { Order } from  "../../domain/payment/order";
 import { Receipt } from  "../../domain/payment/receipt";
 import { checkNotNull } from "../../domain/utils";
 import { HttpMethod } from "../../http/httpmethod";
-import { NotFoundException } from "../../exceptions";
+import { IllegalArgumentException, NotFoundException } from "../../exceptions";
 import { HttpResponseParser } from "../../http/httpresponseparser";
 import { InvalidParameterException, NetworkException, NodeRPCException, ServerUnknownException, UnauthorizedException, VaultForbiddenException } from "../../exceptions";
 
@@ -70,33 +70,31 @@ export class PaymentService extends RestService {
 	 * @throws HiveException The error comes from the hive node.
 	 */
 	public async payOrder(orderId: string, transactionId: string): Promise<Receipt> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-/*
-		try {
-			return paymentAPI.payOrder(orderId, new PayOrderParams(transactionId)).execute().body();
-		} catch (NodeRPCException e) {
-			switch (e.getCode()) {
-				case NodeRPCException.UNAUTHORIZED:
-					throw new UnauthorizedException(e);
-				case NodeRPCException.FORBIDDEN:
-					throw new VaultForbiddenException(e);
-				case NodeRPCException.BAD_REQUEST:
-					throw new InvalidParameterException(e.getMessage());
-				case NodeRPCException.NOT_FOUND:
-					throw new NotFoundException(e);
-				default:
-					throw new ServerUnknownException(e);
-			}
-		} catch (IOException e) {
-			throw new NetworkException(e);
+		checkNotNull(orderId, "Missing order id.");
+		checkNotNull(transactionId, "Missing transaction id.");
+
+		try {	
+			return await this.httpClient.send<Receipt>(`${PaymentService.API_ORDER_ENDPOINT}/${orderId}`, { "transaction_id": transactionId },
+			<HttpResponseParser<Receipt>> {
+				deserialize(content: any): Receipt {
+					let jsonObj = JSON.parse(content);
+					let receipt = new Receipt();
+					receipt.setReceiptId(jsonObj['receipt_id']);
+					receipt.setOrderId(jsonObj['order_id']);
+					receipt.setTransactionId(jsonObj['transaction_id']);
+					receipt.setPricingName(jsonObj['pricing_name']);
+					receipt.setPaidDid(jsonObj['paid_did']);
+					receipt.setElaAmount(jsonObj['ela_amount']);
+					receipt.setProof(jsonObj['proof']);
+
+					return receipt;
+				}
+			},
+			HttpMethod.POST);
+		} 
+		catch (e) {
+			this.handleError(e);
 		}
-*/
 	}
 
 	/**
@@ -106,18 +104,15 @@ export class PaymentService extends RestService {
 	 * @return The details of the order.
 	 * @throws HiveException The error comes from the hive node.
 	 */
-	public async getOrder(orderId: string): Promise<Order> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-/*
-		List<Order> orders = getOrdersInternal("vault", orderId);
-		return orders.get(0);
-*/
+	public async getOrder(subscription: string, orderId: string): Promise<Order> {
+		checkNotNull(subscription, "Missing subscription.");
+		checkNotNull(orderId, "Missing order id.");
+
+		if (subscription != "vault" && subscription != "backup") {
+			throw new IllegalArgumentException("Invalid subscription. Must be 'vault' or 'backup'");
+		}
+
+		return await this.getOrdersInternal(subscription, orderId)[0];
 	}
 
 	/**
@@ -128,44 +123,37 @@ export class PaymentService extends RestService {
 	 * @throws HiveException The error comes from the hive node.
 	 */
 	public async getOrders(subscription: string): Promise<Order[]> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-		//return getOrdersInternal(subscription, null);
+		checkNotNull(subscription, "Missing subscription.");
+		return await this.getOrdersInternal(subscription);
 	}
 
-	private async getOrdersInternal(subscription: string, orderId: string): Promise<Order[]> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-/*
-		try {
-			return paymentAPI.getOrders(subscription, orderId).execute().body().getOrders();
-		} catch (NodeRPCException e) {
-			switch (e.getCode()) {
-				case NodeRPCException.UNAUTHORIZED:
-					throw new UnauthorizedException(e);
-				case NodeRPCException.FORBIDDEN:
-					throw new VaultForbiddenException(e);
-				case NodeRPCException.BAD_REQUEST:
-					throw new InvalidParameterException(e.getMessage());
-				case NodeRPCException.NOT_FOUND:
-					throw new NotFoundException(e);
-				default:
-					throw new ServerUnknownException(e);
-			}
-		} catch (IOException e) {
-			throw new NetworkException(e);
+	private async getOrdersInternal(subscription: string, orderId?: string): Promise<Order[]> {
+		try {	
+			return await this.httpClient.send<Order[]>(PaymentService.API_ORDER_ENDPOINT, { "subscription": subscription, "order_id": orderId },
+			<HttpResponseParser<Order[]>> {
+				deserialize(content: any): Order[] {
+					let jsonObj = JSON.parse(content)['orders'];
+					let orders = [];
+					for (let orderObj in jsonObj) {
+						let order = new Order();
+						order.setCreateTime(orderObj['create_time']);
+						order.setElaAddress(orderObj['ela_address']);
+						order.setElaAmount(orderObj['ela_amount']);
+						order.setOrderId(orderObj['order_id']);
+						order.setPricingName(orderObj['pricina_name']);
+						order.setProof(orderObj['proof']);
+						order.setSubscription(orderObj['subscription']);	
+						orders.push(order);
+					}
+
+					return orders;
+				}
+			},
+			HttpMethod.GET);
+		} 
+		catch (e) {
+			this.handleError(e);
 		}
-*/
 	}
 
 	/**
@@ -176,33 +164,28 @@ export class PaymentService extends RestService {
 	 * @throws HiveException The error comes from the hive node.
 	 */
 	public async getReceipt(orderId: string): Promise<Receipt> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-/*
-		try {
-			return paymentAPI.getReceipt(orderId).execute().body();
-		} catch (NodeRPCException e) {
-			switch (e.getCode()) {
-				case NodeRPCException.UNAUTHORIZED:
-					throw new UnauthorizedException(e);
-				case NodeRPCException.FORBIDDEN:
-					throw new VaultForbiddenException(e);
-				case NodeRPCException.BAD_REQUEST:
-					throw new InvalidParameterException(e.getMessage());
-				case NodeRPCException.NOT_FOUND:
-					throw new NotFoundException(e);
-				default:
-					throw new ServerUnknownException(e);
-			}
-		} catch (IOException e) {
-			throw new NetworkException(e);
+		try {	
+			return await this.httpClient.send<Receipt>(PaymentService.API_RECEIPT_ENDPOINT, { "order_id": orderId },
+			<HttpResponseParser<Receipt>> {
+				deserialize(content: any): Receipt {
+					let jsonObj = JSON.parse(content);
+					let receipt = new Receipt();
+					receipt.setReceiptId(jsonObj['receipt_id']);
+					receipt.setOrderId(jsonObj['order_id']);
+					receipt.setTransactionId(jsonObj['transaction_id']);
+					receipt.setPricingName(jsonObj['pricing_name']);
+					receipt.setPaidDid(jsonObj['paid_did']);
+					receipt.setElaAmount(jsonObj['ela_amount']);
+					receipt.setProof(jsonObj['proof']);
+
+					return receipt;
+				}
+			},
+			HttpMethod.GET);
+		} 
+		catch (e) {
+			this.handleError(e);
 		}
-*/
 	}
 
 	/**
@@ -212,29 +195,18 @@ export class PaymentService extends RestService {
 	 * @throws HiveException The error comes from the hive node.
 	 */
 	public async getVersion(): Promise<string> {
-		return await new Promise((resolve, reject)=>{
-			try {
-				resolve(null);
-			} catch (e) {
-				reject(e);
-			}
-		});
-/*
-		try {
-			return paymentAPI.getVersion().execute().body().getVersion();
-		} catch (NodeRPCException e) {
-			switch (e.getCode()) {
-				case NodeRPCException.UNAUTHORIZED:
-					throw new UnauthorizedException(e);
-				case NodeRPCException.FORBIDDEN:
-					throw new VaultForbiddenException(e);
-				default:
-					throw new ServerUnknownException(e);
-			}
-		} catch (IOException e) {
-			throw new NetworkException(e);
+		try {	
+			return await this.httpClient.send<string>(PaymentService.API_VERSION_ENDPOINT, HttpClient.NO_PAYLOAD,
+			<HttpResponseParser<string>> {
+				deserialize(content: any): string {
+					return JSON.parse(content)['version'];
+				}
+			},
+			HttpMethod.GET);
+		} 
+		catch (e) {
+			this.handleError(e);
 		}
-*/
 	}
 
 	private handleError(e: Error): void {
@@ -254,5 +226,4 @@ export class PaymentService extends RestService {
 		}
 		throw new NetworkException(e.message, e);
 	}
-
 }
