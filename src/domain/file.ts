42517dbd80d3081f7cd332c8af3d0a85b748a3e6
dchagastelles <dchagastelles@gmail.com>
2021-12-07 16:00:51 -0500

    using axios on browser version

--- src/domain/file.ts
+++ src/domain/file.ts
@@ -23,7 +23,7 @@
 import path from "path";
 
 //import { existsSync, mkdirSync, readdirSync, readFileSync, unlinkSync, renameSync, statSync, writeFileSync, lstatSync, rmdirSync } from "./fs";
-import * as fs from "./fs";
+import * as fs from "./fs.browser";
 import { Logger } from "../logger";
 
 
