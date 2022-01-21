b9746d39f4b80d5dc099e8515ba71b3547302ca3
carl.duranleau <carl.duranleau@ticketmaster.com>
2022-01-21 08:49:49 -0500

    Removed Java files

--- src/java/org/elastos/hive/vault/scripting/RunFileParams.java
+++
@@ -1,9 +0,0 @@
-package org.elastos.hive.vault.scripting;
-
-import java.util.HashMap;
-
-class RunFileParams extends HashMap<String, String> {
-    public RunFileParams(String path) {
-        super.put("path", path);
-    }
-}
