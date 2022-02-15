4f8e935d253277a41c54f9d0a12c590f4e887c27
carl.duranleau <carl.duranleau@ticketmaster.com>
2022-02-14 21:23:15 -0500

    Added new testnet configuration.

--- tests/src/config/clientconfig.ts
+++ tests/src/config/clientconfig.ts
@@ -2,12 +2,14 @@ import localConfig from "../res/local.json";
 import devConfig from "../res/developing.json";
 import prodConfig from "../res/production.json";
 import customConfig from "../res/custom.json";
+import testConfig from "../res/testnet.json";
 
 export class ClientConfig {
     public static LOCAL = localConfig;
     public static DEV = devConfig;
     public static PRODUCTION = prodConfig;
     public static CUSTOM = customConfig;
+    public static TEST = testConfig;
 
     private static CURRENT_CONFIG = ClientConfig.LOCAL;
 
