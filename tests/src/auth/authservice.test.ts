4dc4db3db32c648c7a8c39cf1bb8365d8ae8c3ad
carl.duranleau <carl.duranleau@ticketmaster.com>
2022-02-18 16:13:22 -0500

    Added failed authentication test.

--- tests/src/auth/authservice.test.ts
+++ tests/src/auth/authservice.test.ts
@@ -1,4 +1,4 @@
-import { ServiceContext, AuthService } from "@elastosfoundation/elastos-hive-js-sdk";
+import { NodeRPCException, UnauthorizedException, ServerUnknownException, VaultSubscriptionService, ServiceContext, AuthService } from "@elastosfoundation/elastos-hive-js-sdk";
 import { ClientConfig } from "../config/clientconfig";
 import { TestData } from "../config/testdata";
 
@@ -12,8 +12,40 @@ describe("test auth service", () => {
         serviceContext = new ServiceContext(testData.getAppContext(), testData.getProviderAddress());
 	});
 
+	afterAll(() => { serviceContext.getAccessToken().invalidate(); });
+
     test("testAuth", async () => {
 		let token = await serviceContext.getAccessToken().fetch();
 		expect(token).not.toBeNull();
+    });
+});
+
+describe("authentication fail test", () => {
+
+	let testData: TestData;
+    let vaultsubscriptionService: VaultSubscriptionService;
+
+    beforeAll(() => {
+        let spy = jest.spyOn(AuthService.prototype, 'auth').mockImplementation(() => {throw new ServerUnknownException(NodeRPCException.SERVER_EXCEPTION, "Expected error");});
+    });
+
+    beforeEach(async () => {
+        testData = await TestData.getInstance("vault subscribe.test", ClientConfig.CUSTOM, TestData.USER_DIR);
+        vaultsubscriptionService = new VaultSubscriptionService(
+            testData.getAppContext(),
+            testData.getProviderAddress());
+    });
+
+    afterAll(() => { jest.resetAllMocks(); });
+
+	test("testAuthenticationFailed", async () => {
+		let actualError = null;
+        try {
+            await vaultsubscriptionService.getPricingPlanList();
+        } catch (e) {
+			actualError = e;
+        }
+		expect(actualError).not.toBeNull();
+		expect(actualError instanceof UnauthorizedException).toBeTruthy();
     });
 });
