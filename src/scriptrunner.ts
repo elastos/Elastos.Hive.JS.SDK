07adc0bc86a48aaf7c5ed0868341efd47f11182b
Fred Chen <chentj070921@yeah.net>
2022-04-13 16:05:42 +0800

    CU-25ddgzk - Add script runner service and support downloading a file through a script by the hive url.

---
+++ src/scriptrunner.ts
@@ -0,0 +1,37 @@
+import {ServiceContext} from "./connection/servicecontext";
+import {HttpClient} from "./connection/httpclient";
+import {AppContext} from "./connection/auth/appcontext";
+import {ScriptingService} from "./service/scripting/scriptingservice";
+
+
+export class ScriptRunner extends ServiceContext{
+
+    private scriptService: ScriptingService;
+
+    public constructor(context: AppContext, providerAddress?: string) {
+        super(context, providerAddress);
+        const httpClient   = new HttpClient(this, HttpClient.WITH_AUTHORIZATION, HttpClient.DEFAULT_OPTIONS);
+        this.scriptService = new ScriptingService(this, httpClient);
+    }
+
+    public async callScript<T>(name: string, params: any, targetDid: string, targetAppDid: string): Promise<T> {
+        return await this.scriptService.callScript(name, params, targetDid, targetAppDid);
+    }
+
+    public async callScriptUrl<T>(name: string, params: string, targetDid: string, targetAppDid: string): Promise<T> {
+        return await this.scriptService.callScriptUrl(name, params, targetDid, targetAppDid);
+    }
+
+    public async uploadFile(transactionId: string, data: Buffer): Promise<void> {
+        return await this.scriptService.uploadFile(transactionId, data);
+    }
+
+    public async downloadFile(transactionId: string): Promise<Buffer> {
+        return await this.scriptService.downloadFile(transactionId);
+    }
+
+    public async downloadFileByHiveUrl(hiveUrl: string): Promise<Buffer> {
+        return await this.scriptService.downloadFileByHiveUrl(hiveUrl);
+    }
+
+}
