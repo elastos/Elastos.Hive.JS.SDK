9b31eaf35bc8f29d1c8bc122ed209d7a08bd3ec9
Fred Chen <chentj070921@yeah.net>
2022-04-20 12:52:20 +0800

    CU-2j3wcyd - Refine getAppStates API.

--- tests/src/about/aboutservice.test.ts
+++ tests/src/about/aboutservice.test.ts
@@ -26,13 +26,13 @@ describe("test about service", () => {
     test("testGetNodeInfo", async () => {
         const nodeInfo: NodeInfo = await serviceContext.getNodeInfo();
         expect(nodeInfo).not.toBeNull();
-        expect(nodeInfo.service_did).not.toBeNull();
-        expect(nodeInfo.owner_did).not.toBeNull();
-        expect(nodeInfo.ownership_presentation).not.toBeNull();
-        expect(nodeInfo.name).not.toBeNull();
-        expect(nodeInfo.email).not.toBeNull();
-        expect(nodeInfo.description).not.toBeNull();
-        expect(nodeInfo.version).not.toBeNull();
-        expect(nodeInfo.last_commit_id).not.toBeNull();
+        expect(nodeInfo.getServiceDid()).not.toBeNull();
+        expect(nodeInfo.getOwnerDid()).not.toBeNull();
+        expect(nodeInfo.getOwnershipPresentation()).not.toBeNull();
+        expect(nodeInfo.getName()).not.toBeNull();
+        expect(nodeInfo.getEmail()).not.toBeNull();
+        expect(nodeInfo.getDescription()).not.toBeNull();
+        expect(nodeInfo.getVersion()).not.toBeNull();
+        expect(nodeInfo.getLastCommitId()).not.toBeNull();
     });
 });
