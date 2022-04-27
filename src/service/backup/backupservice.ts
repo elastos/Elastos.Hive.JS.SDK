7d49930cb042f94b8c0d9b91fb123dd8a9651dd2
Fred Chen <chentj070921@yeah.net>
2022-04-27 16:27:58 +0800

    CU-2ufeghj - Fix an error about BackupResult to adapt the lower version of typescript.

--- src/service/backup/backupservice.ts
+++ src/service/backup/backupservice.ts
@@ -7,7 +7,7 @@ import { HttpClient } from "../../connection/httpclient";
 import { ServiceContext } from "../../connection/servicecontext";
 import { Logger } from '../../utils/logger';
 import { RestService } from "../restservice";
-import { BackupResult } from "./backupresult";
+import {BackupResult, BackupResultResult, BackupResultState} from "./backupresult";
 import { HttpMethod } from "../../connection/httpmethod";
 import { HttpResponseParser } from '../../connection/httpresponseparser';
 import {CredentialCode} from "./credentialcode";
@@ -59,12 +59,39 @@ export class BackupService extends RestService {
 		});
 	}
 
+	private static getBackupResultStateByStr(value: string): BackupResultState {
+	    if (value === 'stop') {
+	        return BackupResultState.STATE_STOP;
+	    } else if (value === 'backup') {
+			return BackupResultState.STATE_BACKUP;
+	    } else if (value === 'restore') {
+	        return BackupResultState.STATE_RESTORE;
+	    } else {
+	        throw Error('Unknown state.');
+	    }
+	}
+
+	private static getBackupResultStatusByStr(value: string): BackupResultResult {
+		if (value === 'success') {
+			return BackupResultResult.RESULT_SUCCESS;
+		} else if (value === 'failed') {
+			return BackupResultResult.RESULT_FAILED;
+		} else if (value === 'process') {
+			return BackupResultResult.RESULT_PROCESS;
+		} else {
+			throw Error('Unknown result.');
+		}
+	}
+
 	public async checkResult() : Promise<BackupResult> {
 		try {
 			return await this.httpClient.send<BackupResult>(BackupService.API_BACKUP_ENDPOINT, HttpClient.NO_PAYLOAD,
 					<HttpResponseParser<BackupResult>> {
 				deserialize(content: any): BackupResult {
-					return JSON.parse(content) as BackupResult;
+					let json_dict = JSON.parse(content);
+					json_dict['state'] = BackupService.getBackupResultStateByStr(json_dict['state']);
+					json_dict['result'] = BackupService.getBackupResultStatusByStr(json_dict['result']);
+					return Object.assign(new BackupResult(), json_dict);
 				}
 			}, HttpMethod.GET);
 		} catch (e) {
