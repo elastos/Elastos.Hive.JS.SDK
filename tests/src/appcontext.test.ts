e12934f9f1d608de37f8d93975f962c81e9600c3
Fred Chen <chentj070921@yeah.net>
2022-08-18 15:13:52 +0800

    CU-2xhrjqn - Add encryption utils to encrypt the data.

--- tests/src/appcontext.test.ts
+++ tests/src/appcontext.test.ts
@@ -1,4 +1,5 @@
 import {AppContext} from "../../src";
+import {EncryptionUtils} from "../../src/utils/encryption/encrypt_utils";
 const _sodium = require('libsodium-wrappers');
 
 /**
@@ -80,10 +81,33 @@ describe.skip("test appcontext", () => {
         expect(sodium.to_string(msg3.message)).toEqual("three messages");
     });
 
-    test('testKeyPair', async () => {
-        // TODO:
+    test('EncryptionUtils', async () => {
+        const utils = new EncryptionUtils();
+        const key = Uint8Array.from(Buffer.from('5fde3dbee357613c3ecc62fc36c30bdc10f0cd9c6ee8ebea4966754c7e2d403a', 'hex'));
+        const data = '{"@context":["https://www.w3.org/ns/did/v1","https://ns.elastos.org/did/v1","https://w3id.org/security/v1"],"id":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR","publicKey":[{"id":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary","type":"ECDSAsecp256r1","controller":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR","publicKeyBase58":"nQgixgrEngN2xLQsD87mKXwhfAuXuLj94gi4JN2d5zqz"}],"authentication":["did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary"],"expires":"2027-04-01T08:35:47Z","proof":{"type":"ECDSAsecp256r1","created":"2022-04-01T08:35:47Z","creator":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary","signatureValue":"6LTNIME_2PSMppIdNvXXn_ZwjzFpuluyqdLx628ED2Ih6QNQN2C20orZQJA_VxYPmMWENp1oAoa7OgqMR5-IxQ"}}{"@context":["https://www.w3.org/ns/did/v1","https://ns.elastos.org/did/v1","https://w3id.org/security/v1"],"id":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR","publicKey":[{"id":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary","type":"ECDSAsecp256r1","controller":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR","publicKeyBase58":"nQgixgrEngN2xLQsD87mKXwhfAuXuLj94gi4JN2d5zqz"}],"authentication":["did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary"],"expires":"2027-04-01T08:35:47Z","proof":{"type":"ECDSAsecp256r1","created":"2022-04-01T08:35:47Z","creator":"did:elastos:ipUGBPuAgEx6Le99f4TyDfNZtXVT2NKXPR#primary","signatureValue":"6LTNIME_2PSMppIdNvXXn_ZwjzFpuluyqdLx628ED2Ih6QNQN2C20orZQJA_VxYPmMWENp1oAoa7OgqMR5-IxQ"}}';
+        const encryptData = await utils.encryptData(key, Buffer.from(data));
+        console.log(`encoded data: ${encryptData.toString('hex')}`);
+        expect(encryptData).not.toBeNull();
+
+        const decryptData = await utils.decryptData(key, encryptData);
+        expect(decryptData.toString('utf8')).toEqual(data);
     });
 
-    test('testEncryptDecryptByKeyPair', async () => {
+    test('testGenerateEncryptionKeyPair', async () => {
+        await _sodium.ready;
+        const sodium = _sodium;
+
+        // example OK
+        let aliceKeypair = Buffer.from(
+            '411a2c2227d2a799ebae0ed94417d8e8ed1ca9b0a9d5f4cd743cc52d961e94e2' +
+            'da49154c9e700b754199df7974e9fa4ee4b6ebbc71f89d8d8938335ea4a1409d' +
+            'da49154c9e700b754199df7974e9fa4ee4b6ebbc71f89d8d8938335ea4a1409d', 'hex');
+        let aliceSecret = Buffer.from(aliceKeypair.slice(0, 64)); // 64bytes
+        let alicePublic = Buffer.from(aliceKeypair.slice(64, 96)); // 32bytes
+
+        let ecdhSecret = Buffer.from(sodium.crypto_sign_ed25519_sk_to_curve25519(aliceSecret));
+        expect(ecdhSecret.toString('hex')).toEqual('60c783b8d1674b7081b72a105b55872502825d4ec638028152e085b54705ad7e');
+        let ecdhPublic = Buffer.from(sodium.crypto_sign_ed25519_pk_to_curve25519(alicePublic));
+        expect(ecdhPublic.toString('hex')).toEqual('5a791d07cfb39060c8e9b641b6a915a3126cd14ddc243a9928c490c8e1f59e7c');
     });
 });
