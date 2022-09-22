e6ffc21d58735446ce165efdf7cadc58a5e568c3
Fred Chen <chentj070921@yeah.net>
2022-09-22 11:44:33 +0800

    CU-3jm0kmc - Refine the definition of about APIs.

--- src/service/about/aboutapi.ts
+++ src/service/about/aboutapi.ts
@@ -1,23 +1,13 @@
-
 import {BasePath, BaseService, GET, Header, Response} from 'ts-retrofit';
 
 @BasePath("/api/v2")
 export class AboutAPI extends BaseService {
     @GET("/node/version")
-    async version(): Promise<Response> {
-        return await new Promise<Response>((resolve) => {
-            throw new Error('Not implemented');
-        }); }
+    async version(): Promise<Response> { return null; }
 
     @GET("/node/commit_id")
-    async commitId(): Promise<Response> {
-        return await new Promise<Response>((resolve) => {
-            throw new Error('Not implemented');
-        }); }
+    async commitId(): Promise<Response> { return null; }
 
     @GET("/node/info")
-    async info(@Header("Authorization") authorization: string): Promise<Response> {
-        return await new Promise<Response>((resolve) => {
-            throw new Error('Not implemented');
-        }); }
+    async info(@Header("Authorization") authorization: string): Promise<Response> { return null; }
 }
