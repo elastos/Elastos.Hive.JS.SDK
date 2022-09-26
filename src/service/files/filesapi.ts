8609e0b2ab86b24ad7d7e31bd214d0d523c5ad15
Fred Chen <chentj070921@yeah.net>
2022-09-27 00:52:21 +0800

    CU-3jtumwg - Refine files service.

--- src/service/files/filesapi.ts
+++ src/service/files/filesapi.ts
@@ -1,41 +1,111 @@
-import {BasePath, BaseService, Body, DELETE, GET, Header, PATCH, Path, POST, PUT, Query, Response} from 'ts-retrofit';
+import {
+    BasePath,
+    BaseService,
+    Body,
+    Headers,
+    DELETE,
+    GET,
+    Header, Multipart, Part, PartDescriptor,
+    PATCH,
+    Path,
+    POST,
+    PUT,
+    Query,
+    Response,
+    ResponseTransformer, ResponseType, RequestTransformer
+} from 'ts-retrofit';
+import {APIResponse} from "../restservice";
+import {FileInfo} from "../..";
 
 @BasePath("/api/v2/vault")
 export class FilesAPI extends BaseService {
     @GET("/files/{path}")
+    @ResponseType("arraybuffer")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data);
+    })
     async download(@Header("Authorization") authorization: string,
                    @Path("path") path: string): Promise<Response> { return null; }
 
     @PUT("/files/{path}")
+    @RequestTransformer((data: any, headers?: any) => {
+        return Buffer.from(data['data'], 'hex');
+    })
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj["cid"];
+        });
+    })
     async upload(@Header("Authorization") authorization: string,
                  @Query("public") isPublic: boolean,
                  @Query("script_name") scriptName: string,
                  @Query("is_encrypt") isEncrypt: boolean,
                  @Query("encrypt_method") encrypt_method: string,
                  @Path("path") path: string,
-                 @Body body: Buffer): Promise<Response> { return null; }
+                 @Body body: object): Promise<Response> { return null; }
 
     @GET("/files/{path}?comp=children")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            const rawFiles = jsonObj["value"];
+            let files = [];
+            for (let file of rawFiles) {
+                let fileInfo = new FileInfo();
+                fileInfo.setCreated(file["created"]);
+                fileInfo.setUpdated(file["updated"]);
+                fileInfo.setName(file["name"]);
+                fileInfo.setAsFile(file["is_file"]);
+                fileInfo.setSize(file["size"]);
+                files.push(fileInfo);
+            }
+            return files;
+        });
+    })
     async listChildren(@Header("Authorization") authorization: string,
                        @Path("path") path: string): Promise<Response> { return null; }
 
     @GET("/files/{path}?comp=metadata")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            const newFileInfo = new FileInfo();
+            newFileInfo.setCreated(jsonObj["created"]);
+            newFileInfo.setUpdated(jsonObj["updated"]);
+            newFileInfo.setName(jsonObj["name"]);
+            newFileInfo.setAsFile(jsonObj["is_file"]);
+            return newFileInfo;
+        });
+    })
     async getMetadata(@Header("Authorization") authorization: string,
                       @Path("path") path: string): Promise<Response> { return null; }
 
     @GET("/files/{path}?comp=hash")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj['hash'];
+        });
+    })
     async getHash(@Header("Authorization") authorization: string,
                   @Path("path") path: string): Promise<Response> { return null; }
 
     @PUT("/files/{path}")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj['name'];
+        });
+    })
     async copy(@Header("Authorization") authorization: string,
                @Path("path") src: string,
                @Query("dest") dest: string): Promise<Response> { return null; }
 
     @PATCH("/files/{path}")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj['name'];
+        });
+    })
     async move(@Header("Authorization") authorization: string,
                @Path("path") src: string,
-               @Query("to") to: string): Promise<Response> { return null; }
+               @Query("to") dst: string): Promise<Response> { return null; }
 
     @DELETE("/files/{path}")
     async delete(@Header("Authorization") authorization: string,
