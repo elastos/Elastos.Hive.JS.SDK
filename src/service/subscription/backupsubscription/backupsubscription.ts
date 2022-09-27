de8dc52d77ef079e7acd9a1838fd278b56e1f00c
Fred Chen <chentj070921@yeah.net>
2022-09-27 20:22:16 +0800

    CU-3jtunbn - Refine subscription services by ts-retrofit.

--- src/service/subscription/backupsubscription/backupsubscription.ts
+++ src/service/subscription/backupsubscription/backupsubscription.ts
@@ -1,68 +1,65 @@
-import { AppContext } from "../../../connection/auth/appcontext";
-import { HttpClient } from "../../../connection/httpclient";
-import { ServiceEndpoint } from "../../../connection/serviceendpoint";
-import { Logger } from '../../../utils/logger';
-import { PaymentService } from "../../payment/paymentservice";
-import { SubscriptionService } from "../../subscription/subscriptionservice";
-import { PricingPlan } from "../pricingplan";
-import { BackupInfo } from "../backupinfo";
-import { Order } from "../../payment/order";
-import { Receipt } from "../../payment/receipt";
+import {AppContext} from "../../../connection/auth/appcontext";
+import {HttpClient} from "../../../connection/httpclient";
+import {ServiceEndpoint} from "../../../connection/serviceendpoint";
+import {PaymentService} from "../../payment/paymentservice";
+import {SubscriptionService} from "../../subscription/subscriptionservice";
+import {PricingPlan} from "../pricingplan";
+import {BackupInfo} from "../backupinfo";
+import {Order} from "../../payment/order";
+import {Receipt} from "../../payment/receipt";
 
 export class BackupSubscription extends ServiceEndpoint {
-	private static LOG = new Logger("BackupSubscriptionService");
-
     private paymentService: PaymentService;
     private subscriptionService: SubscriptionService;
 
-    public constructor(context: AppContext, providerAddress?: string) {
+    constructor(context: AppContext, providerAddress?: string) {
 		super(context, providerAddress);
         let httpClient = new HttpClient(this, HttpClient.WITH_AUTHORIZATION, HttpClient.DEFAULT_OPTIONS);
         this.paymentService = new PaymentService(this, httpClient);
         this.subscriptionService = new SubscriptionService(this, httpClient);
 	}
 
-	public async getPricingPlanList(): Promise<PricingPlan[]> {
+	async getPricingPlanList(): Promise<PricingPlan[]> {
         return await this.subscriptionService.getBackupPricingPlanList();
     }
 
-	public async getPricingPlan(planName: string): Promise<PricingPlan> {
+	async getPricingPlan(planName: string): Promise<PricingPlan> {
 		return await this.subscriptionService.getBackupPricingPlan(planName);
 	}
 
-	public async subscribe(): Promise<BackupInfo> {
+	async subscribe(): Promise<BackupInfo> {
 		return await this.subscriptionService.subscribeToBackup();
 	}
 
-	public async unsubscribe(): Promise<void> {
+	async unsubscribe(): Promise<void> {
 		return await this.subscriptionService.unsubscribeBackup();
 	}
 
-	public async checkSubscription(): Promise<BackupInfo> {
+	async checkSubscription(): Promise<BackupInfo> {
 		return await this.subscriptionService.getBackupInfo();
 	}
 
-	public async placeOrder(planName: string): Promise<Order> {
+	async placeOrder(planName: string): Promise<Order> {
 		return await this.paymentService.placeOrder("backup", planName);
 	}
 
-	public async getOrder(orderId: number): Promise<Order> {
+	async getOrder(orderId: number): Promise<Order> {
 		return await this.paymentService.getOrder("backup", orderId);
 	}
 
-	public async settleOrder(orderId: number): Promise<Receipt> {
+	async settleOrder(orderId: number): Promise<Receipt> {
 		return await this.paymentService.settleOrder(orderId);
 	}
 
-	public async getOrderList(): Promise<Order[]> {
+	async getOrderList(): Promise<Order[]> {
 		return await this.paymentService.getOrders("backup");
 	}
 
-	public async getReceipt(orderId?: number): Promise<Receipt[]> {
+	async getReceipt(orderId?: number): Promise<Receipt[]> {
 		return await this.paymentService.getReceipts(orderId);
 	}
 
-	public async getVersion(): Promise<string> {
+	async getVersion(): Promise<string> {
 		return await this.paymentService.getVersion();
 	}
 }
