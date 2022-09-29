f992bc0327784f0b6460eac056b40d6af3973422
Fred Chen <chentj070921@yeah.net>
2022-09-29 20:34:58 +0800

    CU-338pa72 - Refine scripting module.

--- src/service/scripting/findexecutable.ts
+++ src/service/scripting/findexecutable.ts
@@ -5,15 +5,15 @@ export class FindExecutableBody extends ExecutableDatabaseBody {
     private filter: any;
     private options: any;
 
-    constructor(collection:string, filter: any, options: any) {
+    constructor(collection:string, filter: any, options?: any) {
         super(collection);
         this.filter = filter;
-        this.options = options;
+        this.options = options ? options : undefined;
     }
 }
 
 export class FindExecutable extends Executable {
-	constructor( name: string,  collectionName: string,  filter: any,  options: any) {
+	constructor( name: string,  collectionName: string,  filter: any,  options?: any) {
 		super(name, ExecutableType.FIND, null);
 		super.setBody(new FindExecutableBody(collectionName, filter, options));
     }
