4f13a81685ec2375743d8a82a503389b2c4a7cb1
Fred Chen <chentj070921@yeah.net>
2022-09-30 11:08:54 +0800

    CU-338pa72 - Refine promotion module.

--- src/service/promotion/promotionservice.ts
+++ src/service/promotion/promotionservice.ts
@@ -1,31 +1,16 @@
-import { HttpMethod } from "../../connection/httpmethod";
-import {
-	NetworkException,
-	NodeRPCException
-} from "../../exceptions";
-import { HttpClient } from "../../connection/httpclient";
-import { ServiceEndpoint } from "../../connection/serviceendpoint";
-import { Logger } from '../../utils/logger';
-import { RestService } from "../restservice";
+import {HttpClient} from "../../connection/httpclient";
+import {ServiceEndpoint} from "../../connection/serviceendpoint";
+import {RestServiceT} from "../restservice";
+import {PromotionAPI} from "./promotionapi";
 
-export class PromotionService extends RestService {
-	private static LOG = new Logger("PromotionService");
-
-	private static API_PROMOTION_ENDPOINT = "/api/v2/backup/promotion";
-
+export class PromotionService extends RestServiceT<PromotionAPI> {
     constructor(serviceContext: ServiceEndpoint, httpClient: HttpClient) {
 		super(serviceContext, httpClient);
 	}
 
-	public async promote(): Promise<void> {
-		try {
-			await this.httpClient.send<void>(PromotionService.API_PROMOTION_ENDPOINT,
-					                         HttpClient.NO_PAYLOAD, HttpClient.NO_RESPONSE, HttpMethod.POST);
-		} catch (e) {
-			if (e instanceof NodeRPCException) {
-				throw e;
-			}
-			throw new NetworkException(e.message, e);
-		}
+	async promote(): Promise<void> {
+        await this.callAPI(PromotionAPI, async api => {
+            return await api.promoteToVault(await this.getAccessToken());
+        });
 	}
 }
