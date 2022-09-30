7b57e1281a2963cc25b1b99ccd9199b218b27299
Fred Chen <chentj070921@yeah.net>
2022-09-30 11:34:34 +0800

    CU-338pa72 - Refine provider module.

---
+++ src/service/provider/providerapi.ts
@@ -0,0 +1,30 @@
+import {BasePath, BaseService, GET, Header, Response, ResponseTransformer} from 'ts-retrofit';
+import {APIResponse} from "../restservice";
+import {BackupDetail, FilledOrderDetail, VaultDetail} from "../..";
+
+@BasePath("/api/v2")
+export class ProviderAPI extends BaseService {
+    @GET("/provider/vaults")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj["vaults"].map(v => Object.assign(new VaultDetail(), v));
+        });
+    })
+    async getVaults(@Header("Authorization") auth: string): Promise<Response> { return null; }
+
+    @GET("/provider/backups")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj["backups"].map(v => Object.assign(new BackupDetail(), v));
+        });
+    })
+    async getBackups(@Header("Authorization") auth: string): Promise<Response> { return null; }
+
+    @GET("/provider/filled_orders")
+    @ResponseTransformer((data: any, headers?: any) => {
+        return APIResponse.handleResponseData(data, (jsonObj) => {
+            return jsonObj["orders"].map(v => Object.assign(new FilledOrderDetail(), v));
+        });
+    })
+    async getFilledOrders(@Header("Authorization") auth: string): Promise<Response> { return null; }
+}
