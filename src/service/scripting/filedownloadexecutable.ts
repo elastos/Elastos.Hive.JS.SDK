8929f7105d4967ba9dbf1e9ecb58519d29fbf4e5
yufei <chentj070921@yeah.net>
2023-09-07 17:27:52 +0800

    update FileDownloadExecutable with specific file name.

--- src/service/scripting/filedownloadexecutable.ts
+++ src/service/scripting/filedownloadexecutable.ts
@@ -4,7 +4,7 @@ import {Executable, ExecutableFileBody, ExecutableType} from "./executable";
  * Used to download file.
  */
 export class FileDownloadExecutable extends Executable {
-    constructor(name: string) {
-        super(name, ExecutableType.FILE_DOWNLOAD, new ExecutableFileBody());
+    constructor(name: string, path?: string) {
+        super(name, ExecutableType.FILE_DOWNLOAD, new ExecutableFileBody(path));
     }
 }
