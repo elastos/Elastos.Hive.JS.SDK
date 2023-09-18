ab2b503d644135f9bdd7cfa3a577b6f52469dcda
yufei <chentj070921@yeah.net>
2023-09-18 17:58:55 +0800

    Replace tab with spaces.

--- tests/src/did/appdid.ts
+++ tests/src/did/appdid.ts
@@ -3,60 +3,60 @@ import dayjs from "dayjs";
 import { DIDEntity } from "./didentity";
 
 export class AppDID extends DIDEntity {
-	public static APP_DID = "appId";
-	public static APP_DID2 = "appDID2";
+    public static APP_DID = "appId";
+    public static APP_DID2 = "appDID2";
 
-	public constructor(name: string, mnemonic: string, phrasepass: string, storepass: string, did: string) {
-		super(name, mnemonic, phrasepass, storepass, did);
-	}
+    public constructor(name: string, mnemonic: string, phrasepass: string, storepass: string, did: string) {
+        super(name, mnemonic, phrasepass, storepass, did);
+    }
 
-	public static async create(name: string, mnemonic: string, phrasepass: string, storepass: string,
-							   resolver: string, storeRoot: string, did?: string): Promise<AppDID> {
+    public static async create(name: string, mnemonic: string, phrasepass: string, storepass: string,
+                               resolver: string, storeRoot: string, did?: string): Promise<AppDID> {
         let newInstance = new AppDID(name, mnemonic, phrasepass, storepass, did);
-		await newInstance.initDid(mnemonic, false, storeRoot);
+        await newInstance.initDid(mnemonic, false, storeRoot);
         return newInstance;
     }
 
-	public getAppDid(): string {
-		return AppDID.APP_DID;
-	}
+    public getAppDid(): string {
+        return AppDID.APP_DID;
+    }
 
-	public async createPresentation(vc: VerifiableCredential, realm: string, nonce: string): Promise<VerifiablePresentation> {
-		let vpb = await VerifiablePresentation.createFor(this.getDid(), null, this.getDIDStore());
-		let vp = await vpb.credentials(vc)
-				.realm(realm)
-				.nonce(nonce)
-				.seal(this.getStorePassword());
+    public async createPresentation(vc: VerifiableCredential, realm: string, nonce: string): Promise<VerifiablePresentation> {
+        let vpb = await VerifiablePresentation.createFor(this.getDid(), null, this.getDIDStore());
+        let vp = await vpb.credentials(vc)
+                .realm(realm)
+                .nonce(nonce)
+                .seal(this.getStorePassword());
 
-		AppDID.LOG.info("VerifiablePresentation:{}", vp.toString());
+        AppDID.LOG.info("VerifiablePresentation:{}", vp.toString());
 
-		let listener = VerificationEventListener.getDefaultWithIdent("isValid");
-		AppDID.LOG.trace("VerifiablePresentation is Valid :{}", await vp.isValid(listener));
-		AppDID.LOG.trace("Listener :{}", listener.toString());
+        let listener = VerificationEventListener.getDefaultWithIdent("isValid");
+        AppDID.LOG.trace("VerifiablePresentation is Valid :{}", await vp.isValid(listener));
+        AppDID.LOG.trace("Listener :{}", listener.toString());
 
-		return vp;
-	}
+        return vp;
+    }
 
-	public async createToken(vp: VerifiablePresentation, hiveDid: string): Promise<string> {
+    public async createToken(vp: VerifiablePresentation, hiveDid: string): Promise<string> {
         let cal = dayjs();
         let iat = cal.unix();
         let nbf = cal.unix();
         let exp = cal.add(3, 'month').unix();
 
-		// Create JWT token with presentation.
-		let doc: DIDDocument = await this.getDocument();
-		let token = await doc.jwtBuilder()
-				.addHeader(JWTHeader.TYPE, JWTHeader.JWT_TYPE)
-				.addHeader("version", "1.0")
-				.setSubject("DIDAuthResponse")
-				.setAudience(hiveDid)
-				.setIssuedAt(iat)
-				.setExpiration(exp)
-				.setNotBefore(nbf)
-				.claimsWithJson("presentation", vp.toString(true))
-				.sign(this.storepass);
+        // Create JWT token with presentation.
+        let doc: DIDDocument = await this.getDocument();
+        let token = await doc.jwtBuilder()
+                .addHeader(JWTHeader.TYPE, JWTHeader.JWT_TYPE)
+                .addHeader("version", "1.0")
+                .setSubject("DIDAuthResponse")
+                .setAudience(hiveDid)
+                .setIssuedAt(iat)
+                .setExpiration(exp)
+                .setNotBefore(nbf)
+                .claimsWithJson("presentation", vp.toString(true))
+                .sign(this.storepass);
 
-		AppDID.LOG.info("JWT Token: {}", token);
-		return token;
-	}
+        AppDID.LOG.info("JWT Token: {}", token);
+        return token;
+    }
 }
--- tests/src/did/userdid.ts
+++ tests/src/did/userdid.ts
@@ -4,67 +4,67 @@ import { AppDID } from "./appdid";
 import { DIDEntity } from "./didentity";
 
 export class UserDID extends DIDEntity {
-	private issuer: Issuer;
+    private issuer: Issuer;
 
-	constructor(name: string, mnemonic: string, phrasepass: string, storepass: string, did?: string) {
-		super(name, mnemonic, phrasepass, storepass, did);
-	}
+    constructor(name: string, mnemonic: string, phrasepass: string, storepass: string, did?: string) {
+        super(name, mnemonic, phrasepass, storepass, did);
+    }
 
-	public static async create(name: string, mnemonic: string, phrasepass: string, storepass: string,
-		storeRoot: string, did?: string): Promise<UserDID> {
-		let newInstance = new UserDID(name, mnemonic, phrasepass, storepass, did);
-		await newInstance.initDid(mnemonic, true, storeRoot);
-		let doc = await newInstance.getDocument();
-		newInstance.setIssuer(await Issuer.create(doc));
-		return newInstance;
-	}
+    public static async create(name: string, mnemonic: string, phrasepass: string, storepass: string,
+        storeRoot: string, did?: string): Promise<UserDID> {
+        let newInstance = new UserDID(name, mnemonic, phrasepass, storepass, did);
+        await newInstance.initDid(mnemonic, true, storeRoot);
+        let doc = await newInstance.getDocument();
+        newInstance.setIssuer(await Issuer.create(doc));
+        return newInstance;
+    }
 
-	private setIssuer(issuer: Issuer): void {
-		this.issuer = issuer;
-	}
+    private setIssuer(issuer: Issuer): void {
+        this.issuer = issuer;
+    }
 
-	public async issueDiplomaFor(appInstanceDid: AppDID, appDid?: string): Promise<VerifiableCredential> {
-		let subject = {};
+    public async issueDiplomaFor(appInstanceDid: AppDID, appDid?: string): Promise<VerifiableCredential> {
+        let subject = {};
 
-		subject["appDid"] = appDid ? appDid : AppDID.APP_DID;
-		// subject["appInstanceDid"] = appInstanceDid.getDid();
+        subject["appDid"] = appDid ? appDid : AppDID.APP_DID;
+        // subject["appInstanceDid"] = appInstanceDid.getDid();
 
-		let cal = dayjs();
-		cal = cal.add(5, 'year');
+        let cal = dayjs();
+        cal = cal.add(5, 'year');
 
-		let cb = this.issuer.issueFor(appInstanceDid.getDid());
-		let vc = await cb.id(DIDURL.from('#app-id-credential', appInstanceDid.getDid()) as DIDURL)
-			.type("AppIdCredential")
-			.properties(subject)
-			.expirationDate(cal.toDate())
-			.seal(this.getStorePassword());
+        let cb = this.issuer.issueFor(appInstanceDid.getDid());
+        let vc = await cb.id(DIDURL.from('#app-id-credential', appInstanceDid.getDid()) as DIDURL)
+            .type("AppIdCredential")
+            .properties(subject)
+            .expirationDate(cal.toDate())
+            .seal(this.getStorePassword());
 
-		UserDID.LOG.debug("VerifiableCredential: {}", vc.toString());
-		UserDID.LOG.trace("VerifiableCredential IsValid: {}", vc.isValid());
-		return vc;
-	}
+        UserDID.LOG.debug("VerifiableCredential: {}", vc.toString());
+        UserDID.LOG.trace("VerifiableCredential IsValid: {}", vc.isValid());
+        return vc;
+    }
 
-	public async issueBackupDiplomaFor(sourceDID: string, targetHost: string, targetDID: string): Promise<VerifiableCredential> {
-		let subject = {};
-		subject["sourceHiveNodeDID"] = sourceDID;
-		subject["targetHiveNodeDID"] = targetDID;
-		subject["targetNodeURL"] = targetHost;
+    public async issueBackupDiplomaFor(sourceDID: string, targetHost: string, targetDID: string): Promise<VerifiableCredential> {
+        let subject = {};
+        subject["sourceHiveNodeDID"] = sourceDID;
+        subject["targetHiveNodeDID"] = targetDID;
+        subject["targetNodeURL"] = targetHost;
 
-		let cal = dayjs();
-		cal = cal.add(5, 'year');
+        let cal = dayjs();
+        cal = cal.add(5, 'year');
 
-		// TODO: remove this line.
-		this.setIssuer(await Issuer.create(await this.getDocument()));
+        // TODO: remove this line.
+        this.setIssuer(await Issuer.create(await this.getDocument()));
 
-		let cb = this.issuer.issueFor(sourceDID);
-		let vc = await cb.id("hive-backup-credential")
-			.types("HiveBackupCredential", "VerifiableCredential")
-			.properties(subject)
-			.expirationDate(cal.toDate())
-			.seal(this.getStorePassword());
+        let cb = this.issuer.issueFor(sourceDID);
+        let vc = await cb.id("hive-backup-credential")
+            .types("HiveBackupCredential", "VerifiableCredential")
+            .properties(subject)
+            .expirationDate(cal.toDate())
+            .seal(this.getStorePassword());
 
-		UserDID.LOG.info("BackupCredential: {}", vc.toString());
+        UserDID.LOG.info("BackupCredential: {}", vc.toString());
 
-		return vc;
-	}
+        return vc;
+    }
 }
